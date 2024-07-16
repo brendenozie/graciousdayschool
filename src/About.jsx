eb839731619eea438b584ec353fd41241a1185ce
brendenozie <brendenozie@gmail.com>
2024-07-16 14:43:51 +0300

    Initial commit

---
+++ src/About.jsx
@@ -0,0 +1,243 @@
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/header";
+// import { Features } from "./components/features";
+// import { About } from "./components/about";
+// import { Services } from "./components/services";
+// import { Gallery } from "./components/gallery";
+// import { Testimonials } from "./components/testimonials";
+// import { Team } from "./components/Team";
+// import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+import { Carousel } from "./components/courousel";
+
+export const scroll = new SmoothScroll('a[href*="/"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const About = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <>
+        <div id="overviews" class="section lb">
+        <div class="container">
+            <div class="section-title row text-center">
+                <div class="col-md-8 offset-md-2">
+                    <h3>About</h3>
+                    <p class="lead">A learning  centre in komarocks sector one, Embakasi East in Nairobi Kenya which offers quality 
+and affordable education  from Early Childhood  to primary level to the  low class and vulnerable children 
+in the community.</p>
+                </div>
+            </div>{/* end title */}
+        
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="message-box">
+                        <h4>Started in 2004</h4>
+                        <h2>Welcome to Gracious Day education centre</h2>
+                        <p>A learning  centre in komarocks sector one, Embakasi East in Nairobi Kenya which offers quality 
+and affordable education  from Early Childhood  to primary level to the  low class and vulnerable children 
+in the community.</p>
+
+                        <p> To impact sound knowledge and firm foundation to learners which will enable them to acquire good personality and
+ give back to the community positively. </p>
+
+                        <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+                    </div>{/* end messagebox */}
+                </div>{/* end col */}
+				
+				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="post-media wow fadeIn">
+                        <img src="images/about_02.jpg" alt="" class="img-fluid img-rounded"/>
+                    </div>{/* end media */}
+                </div>{/* end col */}
+			</div>
+			<div class="row align-items-center">
+				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="post-media wow fadeIn">
+                        <img src="images/about_03.jpg" alt="" class="img-fluid img-rounded"/>
+                    </div>{/* end media */}
+                </div>{/* end col */}
+				
+				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="message-box">
+                        <h2>Gracious Day School was started  in May, 2004 with 7pupils.</h2>
+                        <p>Our performance has always shown an upward trend over the years since 2012.
+We offer free1 feeding programme for  playgroup pupils ,free french and computer  lessons from grade 1 to 8.
+</p>
+
+                        <p> We have very 
+qualified staff which is well conversant with the new CBC.</p>
+
+                        <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+                    </div>{/* end messagebox */}
+                </div>{/* end col */}
+				
+            </div>{/* end row */}
+        </div>{/* end container */}
+    </div>{/* end section */}
+	
+	<div class="hmv-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-12">
+					<div class="inner-hmv">
+						<div class="icon-box-hmv"><i class="flaticon-achievement"></i></div>
+						<h3>Mission</h3>
+						<div class="tr-pa">M</div>
+						<p>To impact sound knowledge and firm foundation to learners which will enable them to acquire good personality and
+ give back to the community positively.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-12">
+					<div class="inner-hmv">
+						<div class="icon-box-hmv"><i class="flaticon-eye"></i></div>
+						<h3>Vision</h3>
+						<div class="tr-pa">V</div>
+						<p>To be a dynamic center of excellence which nurture young ones holistically and  
+enable  them to be accepted in the community.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-12">
+					<div class="inner-hmv">
+						<div class="icon-box-hmv"><i class="flaticon-history"></i></div>
+						<h3>History</h3>
+						<div class="tr-pa">H</div>
+						<p>Gracious Day School was started  in May, 2004 with 7pupils. 
+Our performance has always shown an upward trend over the years since 2012.
+We offer free feeding programme for  playgroup pupils ,free french and computer  lessons from grade 1 to 8.
+We have very 
+qualified staff which is well conversant with the new CBC.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <div id="testimonials" class="parallax section db parallax-off" style="background-image:url('images/parallax_04.jpg');">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>Testimonials</h3>
+                <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>{/* end title */}
+
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="testi-carousel owl-carousel owl-theme">
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_01.png" alt="" class="img-fluid"/>
+                                <h4>James Fernando </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+                                <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>
+                        {/* end testimonial */}
+
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_02.png" alt="" class="img-fluid"/>
+                                <h4>Jacques Philips </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>
+                        {/* end testimonial */}
+
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_03.png" alt="" class="img-fluid "/>
+                                <h4>Venanda Mercy </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>
+                        {/* end testimonial */}
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_01.png" alt="" class="img-fluid"/>
+                                <h4>James Fernando </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+                                <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>
+                        {/* end testimonial */}
+
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_02.png" alt="" class="img-fluid"/>
+                                <h4>Jacques Philips </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>
+                        {/* end testimonial */}
+
+                        <div class="testimonial clearfix">
+							<div class="testi-meta">
+                                <img src="images/testi_03.png" alt="" class="img-fluid"/>
+                                <h4>Venanda Mercy </h4>
+                            </div>
+                            <div class="desc">
+                                <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+                            </div>
+                            {/* end testi-meta */}
+                        </div>{/* end testimonial */}
+                    </div>{/* end carousel */}
+                </div>
+                {/* {/* end col */}
+            </div>{/* end row */}
+        </div>{/* end container */}
+    </div>{/* end section */}
+
+    <div class="parallax section dbcolor">
+        <div class="container">
+            <div class="row logos">
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_01.png" alt="" class="img-repsonsive"/></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_02.png" alt="" class="img-repsonsive"/></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_03.png" alt="" class="img-repsonsive"/></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_04.png" alt="" class="img-repsonsive"/></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_05.png" alt="" class="img-repsonsive"/></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="images/logo_06.png" alt="" class="img-repsonsive"/></a>
+                </div>
+            </div>{/* end row */}
+        </div>{/* end container */}
+    </div>{/* end section */}
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/navigation.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+export const Navigation = (props) => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="index.html">
+					<img src="images/logo.png" alt="" />
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-host">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="/about">About Us</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="course-grid-4.html">Course </a>
+							
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="blog.html" >Blog </a>
+							
+						</li>
+						<li class="nav-item"><a class="nav-link" href="teachers.html">Teachers</a></li>
+						<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+					</ul>
+					
+				</div>
+			</div>
+		</nav>
+    // <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+    //   <div className="container">
+    //     <div className="navbar-header">
+    //       <button
+    //         type="button"
+    //         className="navbar-toggle collapsed"
+    //         data-toggle="collapse"
+    //         data-target="#bs-example-navbar-collapse-1"
+    //       >
+    //         {" "}
+    //         <span className="sr-only">Toggle navigation</span>{" "}
+    //         <span className="icon-bar"></span>{" "}
+    //         <span className="icon-bar"></span>{" "}
+    //         <span className="icon-bar"></span>{" "}
+    //       </button>
+    //       <a className="navbar-brand page-scroll" href="#page-top">
+    //         React Landing Page
+    //       </a>{" "}
+    //     </div>
+
+    //     <div
+    //       className="collapse navbar-collapse"
+    //       id="bs-example-navbar-collapse-1"
+    //     >
+    //       <ul className="nav navbar-nav navbar-right">
+    //         <li>
+    //           <a href="#features" className="page-scroll">
+    //             Features
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#about" className="page-scroll">
+    //             About
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#services" className="page-scroll">
+    //             Services
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#portfolio" className="page-scroll">
+    //             Gallery
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#testimonials" className="page-scroll">
+    //             Testimonials
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#team" className="page-scroll">
+    //             Team
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="#contact" className="page-scroll">
+    //             Contact
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+  );
+};
